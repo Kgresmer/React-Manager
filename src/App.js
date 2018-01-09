d08ccc3b7319e74b42af5afb06f2be6561492b05
kgresmer <kevin_gresmer@optum.com>
2018-01-09 07:48:26 -0600

    Added the common components

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import {
 } from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import reducers from './reducers';
+import firebase from 'firebase';
 
 
 export default class App extends Component<{}> {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyB365jMu5F9G6dcmtTNJNeAubKu0vDz3L4",
+            authDomain: "reactmanager-7d606.firebaseapp.com",
+            databaseURL: "https://reactmanager-7d606.firebaseio.com",
+            projectId: "reactmanager-7d606",
+            storageBucket: "",
+            messagingSenderId: "297100329819"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View style={styles.container}>
                     <Text>
                         Welcome to React Native!
