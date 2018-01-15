e286762c0c091eba174e4b3b8cc8bb885f04e490
kgresmer <kgresmer@gmail.com>
2018-01-15 14:33:20 -0600

    Created actions and reducers for employee form

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,9 @@
-
+// Login auth stuff
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
 export const LOGIN_USER_FAIL = 'LOGIN_USER_FAIL';
 export const LOGIN_USER = 'LOGIN_USER';
+
+// Employee form
+export const EMPLOYEE_UPDATE = 'EMPLOYEE_UPDATE';
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {Card, CardSection, Input, Button } from "./common";
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Phillip"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        placeholder="555-555-5555"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    // date thing
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         )
     }
 }
